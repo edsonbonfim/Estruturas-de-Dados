aa0f23827c6adab11f4ca91b96756e6ea0270c54
EdsonOnildoJR <inbox.edsononildo@gmail.com>
2018-04-24 20:36:43 -0300

    Implementadas as funcoes pedidas para arvore geral (ainda com alguns erros)

--- ArvoreGeral/main.c
+++ ArvoreGeral/main.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
-typedef struct arvoreGeral
+//Estrutura da arvore geral
+typedef struct arvore
 {
     int info;
-    struct arvoreGeral *filho;
-    struct arvoreGeral *irmao;
-} arvoreGeral;
+    struct arvore *filho;
+    struct arvore *irmao;
+} arvore;
 
-arvoreGeral *ler(arvoreGeral *a, FILE *arq)
+//Criar arvore
+arvore *criar(arvore *a, FILE *arq)
 {
     int num;
     char ch;
@@ -18,237 +21,311 @@ arvoreGeral *ler(arvoreGeral *a, FILE *arq)
     fscanf(arq, "%d", &num);
 
     if (num == -1)
-    {
         a = NULL;
-    }
-
     else
     {
-        a = (arvoreGeral*) malloc(sizeof(arvoreGeral));
+        a = (arvore*) malloc(sizeof(arvore));
         a->info = num;
-        a->filho = ler(a->filho, arq);
-        a->irmao = ler(a->irmao, arq);
+        a->filho = criar(a->filho, arq);
+        a->irmao = criar(a->irmao, arq);
     }
 
     fscanf(arq, "%c", &ch);
+}
+
+//Altura da arvore
+int altura(arvore *a)
+{
+    if (a == NULL)
+        return 0;
+    else
+    {
+        int hf = altura(a->filho) + 1;
+        int hi = altura(a->irmao);
+
+        if (hf > hi)
+            return hf;
+        else
+            return hi;
+    }
+}
+
+//Conta as folhas da arvore
+int contarFolhas (arvore *a)
+{
+    static int cont = 0;
+
+    if (a != NULL)
+    {
+        if (a->filho == NULL)
+        {
+            cont++;
+        }
+        else
+        {
+            contarFolhas(a->filho);
+            contarFolhas(a->irmao);
+        }
+    }
+}
+
+//Verificar se um no existe na arvore
+bool existe(arvore *a, int num)
+{
+    if (a != NULL)
+    {
+        if (a->info == num)
+            return true;
+        else
+        {
+            return existe(a->filho, num) && existe(a->irmao, num);
+        }
+    }
+
+    return false;
+}
+
+arvore *inserir(arvore *a, int x, int y)
+{
+    if (a != NULL)
+    {
+        if (a->info == y)
+        {
+            if (a->filho == NULL)
+            {
+                a->filho = (arvore *) malloc(sizeof(arvore));
+                a->filho->info = x;
+                a->filho->filho = NULL;
+                a->filho->irmao = NULL;
+            }
+
+            else
+            {
+                arvore *aux = (arvore *) malloc(sizeof(arvore));
+                aux->info = x;
+                aux->filho = NULL;
+                aux->irmao = a->irmao;
+                a = aux;
+            }
+        }
+
+        else
+        {
+            a = inserir(a, x, y);
+        }
+    }
 
     return a;
 }
 
-void imprimirPreOrdem(arvoreGeral *a)
+int nivelDoNo(arvore *a, int elem, int cont)
+{
+    int t1,t2;
+
+    if (a == NULL)
+    {
+        return 0;
+    }
+
+    else
+    {
+        if (a->info == elem)
+        {
+            return cont;
+        }
+        else
+        {
+            t1 = nivelDoNo(a->filho, elem, cont+1);
+            t2 = nivelDoNo(a->irmao, elem, cont);
+            if (t1 == 0 || t2 == 0)
+            {
+                return t1 + t2;
+            }
+            else
+            {
+                return t1;
+            }
+        }
+    }
+}
+
+//Imprimir Pre Ordem
+void preOrdem(arvore *a)
 {
     if (a != NULL)
     {
         printf("%d ", a->info);
-        imprimirPreOrdem(a->filho);
-        imprimirPreOrdem(a->irmao);
+        preOrdem(a->filho);
+        preOrdem(a->irmao);
     }
 }
 
-void imprimirEmOrdem (arvoreGeral *a)
+//Imprimir Em Ordem
+void emOrdem(arvore *a)
 {
     if (a != NULL)
     {
-        imprimirEmOrdem(a->filho);
+        emOrdem(a->filho);
         printf("%d ", a->info);
-        imprimirEmOrdem(a->irmao);
+        emOrdem(a->irmao);
     }
 }
 
-void imprimirPosOrdem(arvoreGeral *a)
+//Imprimir Pos Ordem
+void posOrdem(arvore *a)
 {
     if (a != NULL)
     {
-        imprimirPosOrdem(a->filho);
-        imprimirPosOrdem(a->irmao);
+        posOrdem(a->filho);
+        posOrdem(a->irmao);
         printf("%d ", a->info);
     }
 }
 
-void imprimirNivel (arvoreGeral *a, int nivel, int cont)
+
+//Imprimir em Notacao de Parenteses
+void imprimirNotacao(arvore *a)
+{
+    printf("(");
+
+    if (a == NULL)
+        printf("-1");
+    else
+    {
+        printf("%d ", a->info);
+        imprimirNotacao(a->filho);
+        imprimirNotacao(a->irmao);
+    }
+
+    printf(")");
+}
+
+//Imprimir Um Nivel da arvore
+void imprimirNivel(arvore *a, int nivel, int cont)
 {
     if (a != NULL)
     {
         if (cont == nivel)
         {
             printf("%d ", a->info);
+            imprimirNivel(a->irmao, nivel, cont+1);
+        } else{
+            imprimirNivel(a->filho, nivel, cont+1);
+            imprimirNivel(a->irmao, nivel, cont+1);
         }
-        else
+    }
+}
+
+//Imprimir a arvore em Largura
+void imprimirLargura(arvore *a, int h)
+{
+    int i;
+    for (i =0; i<h; i++)
+        imprimirNivel(a, i, 1);
+}
+
+//Imprimir um No Interno
+void imprimirNoInterno(arvore *a)
+{
+    if (a != NULL)
+    {
+        if (a->filho != NULL)
         {
-            imprimirNivel(a->filho, nivel, cont + 1);
-            imprimirNivel(a->irmao, nivel, cont + 1);
-        }
-    }
-}
-
-void imprimirEmLargura(arvoreGeral *arvore, int h)
-{
-    static int nivel = 0;
-
-    if (nivel <= h)
-    {
-        nivel++;
-        imprimirNivel(arvore, nivel, 1);
-        printf("\n");
-        imprimirEmLargura(arvore, h);
-    }
-}
-
-void imprimir(arvoreGeral *a)
-{
-    printf("(");
-
-    if (a != NULL)
-    {
-        printf("%d", a->info);
-        imprimir(a->filho);
-        imprimir(a->irmao);
-    }
-    else
-    {
-        printf("-1");
-    }
-
-    printf(")");
-}
-
-void imprimirFolha(arvoreGeral *a)
-{
-    if (a != NULL)
-    {
-        if (a->filho == NULL && a->irmao == NULL)
             printf("%d ", a->info);
-
+        }
         else
         {
-            imprimirFolha(a->filho);
-            imprimirFolha(a->irmao);
+            imprimirNoInterno(a->filho);
+            imprimirNoInterno(a->irmao);
         }
     }
 }
 
-int existe(arvoreGeral *a, int elem)
+void menu()
 {
-    if (a == NULL)
-        return 0;
-
-    if (a->info == elem)
-        return 1;
-
-    if (existe(a->filho, elem))
-        return 1;
-
-    if (existe(a->irmao, elem))
-        return 1;
-}
-
-int somar(arvoreGeral *a)
-{
-    if (a == NULL)
-    {
-        return 0;
-    }
-    else
-    {
-        return a->info + somar(a->filho) + somar(a->irmao);
-    }
-}
-
-int contarElemento(arvoreGeral *a, int elem)
-{
-    if (a == NULL)
-        return 0;
-
-    if (a->info == elem)
-        return 1 + contarElemento(a->filho, elem) + contarElemento(a->irmao, elem);
-    else
-        return contarElemento(a->filho, elem) + contarElemento(a->irmao, elem);
-}
-
-int contar(arvoreGeral *a)
-{
-    if (a == NULL)
-        return 0;
-
-    return 1 + contar(a->filho) + contar(a->irmao);
-}
-
-int contarNoInterno(arvoreGeral *a)
-{
-    if (a == NULL)
-        return 0;
-
-    if (a->filho == NULL && a->irmao == NULL)
-        return 0;
-    else
-        return 1 + contarNoInterno(a->filho) + contarNoInterno(a->irmao);
-
-}
-
-int altura(arvoreGeral *a)
-{
-    int hd, he;
-
-    if (a == NULL)
-        return 0;
-
-    he = altura(a->filho);
-    hd = altura(a->irmao);
-
-    if (he > hd)
-        return he + 1; //conferir
-    else
-        return hd + 1;
-}
-
-int contaFolha (arvoreGeral *a)
-{
-    if (a == NULL)
-        return 0;
-
-    if (a->filho == NULL && a->irmao == NULL)
-        return 1;
-
-    else
-        return contaFolha(a->filho) + contaFolha(a->irmao);
-}
-
-void liberar(arvoreGeral *a)
-{
-    if (a != NULL)
-    {
-        liberar(a->filho);
-        liberar(a->irmao);
-        free(a);
-    }
-
-    a = NULL;
+    printf("Digite a opcao desejada: \n");
+    printf("\n1 - Imprimir em Notacao de Parenteses\n");
+    printf("2 - Imprimir em Largura\n");
+    printf("3 - Imprimir os nos internos\n");
+    printf("4 - Imprimir o nivel de um no\n");
+    printf("5 - Altura da Arvore\n");
+    printf("6 - Numero de Folhas\n");
+    printf("7 - Inserir um no x como filho do no y\n");
 }
 
 int main(void)
 {
-    FILE *arq;
-    char entrada[21];
+    char entrada[20];
 
     printf("Digite o nome do arquivo: ");
     scanf("%s", entrada);
 
-    if (strstr(entrada, ".txt") == NULL)
-        strcat(entrada, ".txt");
-
-    if (!(arq = fopen(entrada, "r")))
+    if (!strstr(entrada, ".txt"))
     {
-        printf("Nao foi possivel abrir o arquivo %s\n", entrada);
-        return -1;
+        strcat(entrada, ".txt");
     }
 
-    arvoreGeral *arvore = NULL;
+    FILE *arq = fopen(entrada, "r");
 
-    arvore = ler(arvore, arq);
+    if (arq == NULL)
+    {
+        printf("Nao foi possivel abrir o arquivo!\n");
+        exit(1);
+    }
 
-    fclose(arq);
+    arvore *a = criar(a, arq);
 
-    imprimirEmOrdem(arvore);
+    if (a != NULL)
+    {
+        int opcao, elem, x, y;
 
-    return 0;
+        menu();
+
+        scanf("%d", &opcao);
+
+        while (1)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    imprimirNotacao(a);
+                    break;
+
+                case 2:
+                    imprimirLargura(a, altura(a));
+                    break;
+
+                case 3:
+                    imprimirNoInterno(a);
+                    break;
+
+                case 4:
+                    printf("Digite um elemento: ");
+                    scanf("%d", &elem);
+                    nivelDoNo(a, elem, 1);
+                    break;
+
+                case 5:
+                    printf("A Arvore tem altura: %d", altura(a));
+                    break;
+
+                case 6:
+                    printf("A Arvore possui %d folhas\n", contarFolhas(a));
+                    break;
+
+                case 7:
+                    printf("Digite um elemento para ser inserido: ");
+                    scanf("%d", &x);
+                    printf("Digite o no y: ");
+                    scanf("%d", &y);
+
+                default:
+                    exit(1);
+            }
+        }
+    }
+    else
+    {
+        exit(1);
+    }
 }
